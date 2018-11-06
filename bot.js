e6dbbd04edbd9dd271f9f2a31b52dbfe749a0897
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2018-11-06 04:38:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,41 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
- client.user.setActivity("$$help||$$inv",{type: 'WATCHING'})
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+    console.log('This All Accounts Ready');
 });
 
-client.login(process.env.BOT_TOKEN); 
+
+
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.rbbn);
+client2.login(process.env.rbbn2);
+client3.login(process.env.rbbn3);
+client4.login(process.env.rbbn4);
+client5.login(process.env.rbbn5);
+client6.login(process.env.rbbn6);
+client7.login(process.env.rbbn7);
+client8.login(process.env.rbbn8);
+client9.login(process.env.rbbn9);
+client10.login(process.env.rbbn10);
+client11.login(process.env.rbbn11);
+client12.login(process.env.rbbn12);
+client13.login(process.env.rbbn13);
+client14.login(process.env.rbbn14);
+client15.login(process.env.rbbn15);
